94845a1513e9b990be0fe9fa3b7e2700a3de24b9
Mogball <jeffniu22@gmail.com>
2018-10-13 15:03:30 -0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <MCP2515.h>
+#include <Cosa/MCP2515.h>
+#include <Cosa/UART.hh>
+
+#define uprintf(...) { \
+    int __wrt = sprintf(s_buf, __VA_ARGS__); \
+    uart.write(s_buf, __wrt); }
+
+using namespace wlp;
+
+static cosa::MCP2515 s_canBase(Board::D10);
+static MCP2515 s_canBus(&s_canBase);
+static char s_buf[64];
+
+void setup() {
+    uart.begin(9600);
+    while (Result::OK != s_canBus.begin(CAN_500KBPS, MCP_16MHz)) {
+        uprintf("Failed to init CAN -> retrying\n");
+        delay(100);
+    }
+    uprintf("CAN Inited successfully\n");
+}
+
+void loop() {
+    delay(500);
+    uprintf("_loop_\n");
+}
